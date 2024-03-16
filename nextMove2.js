c942edd778bfb95455a0e17928a74a019e34c517
hippo333 <junk.mail.65.hippo@gmail.com>
2024-03-16 14:48:06 +0100

    now rain tel the color

--- nextMove2.js
+++ nextMove2.js
@@ -4,6 +4,8 @@ var [bigBall,lstBigBall] = require('./bigBall');
 var move = require('./move');
 var raining = require('./raining');
 var emptyBotle = require('./emptyBotle');
+let lstColor = [ "DEPTH" ,"WHITE" ,"blue" ,"lightblue" ,"lightgreen" ,"yellow" ,"brightpink" ,"red" ,"green","grey" ,"purple" ,"pink" ,"cyan" ,"orange" ];
+
 
 
 function internMove(){
@@ -92,87 +94,6 @@ function internMove(){
 	
 	}
 
-/*
-	//list all place we can take ball
-	let targets = columns.filter(
-		tgt => tgt.length <4
-	);
-	
-	let btls =[];
-	
-	for (tgt in targets){
-		let theBtl = targets[tgt];
-		let thebll = theBtl[theBtl.length-1];
-		let to = columns.indexOf(theBtl);
-		
-		
-		btls = columns.filter(
-			bll=> bll.length > 0
-			&& bll[bll.length -1]==thebll
-			&& lstBigBall[columns.indexOf(bll)][1] == 0
-			&& bll != theBtl
-			&& theBtl.length +lstBigBall[columns.indexOf(bll)][0] <=4
-		);
-		
-		// 1 grade
-		//x000
-		//xxx
-		//
-		if(emptyBtl != null && lstBigBall[to][1]!=0){
-			btls2 = columns.filter(
-				bll=> bll.length > 0
-				&& bll[bll.length -1-lstBigBall[columns.indexOf(bll)][0]]==thebll
-				&& bll.length == lstBigBall[columns.indexOf(bll)][0] +1
-			);
-			if(btls2.length >0){
-				//console.log("first grade ,",btls2,to);
-			}
-		}
-		//
-		
-		if (btls == undefined){
-			continue;
-		}
-		
-		//for all btl with the same above ball
-		
-		for(bll in btls){
-			let btl2 = btls[bll];
-			let from = columns.indexOf(btl2);
-			
-			//inevitable
-			if(lstBigBall[to][1] != 0){
-				return [[from,to]]
-			}
-			
-			//first grad
-			//x000
-			//xxx
-			//
-			
-			
-			//asymetrie
-			if(columns[from].length ==1 && columns[to].length == lstBigBall[to][0]){
-				continue;
-			}else if (columns[from].length == columns[to].length && from > to){
-				continue;
-			}
-			
-			
-			
-			
-			
-			//over flow
-			if(theBtl.length +lstBigBall[from][0] <=4){
-				out.push([from,to]);
-				
-			}
-		}
-	}
-	*/
-	
-	
-	
 	//rain if fail
 	if (out.length == 0){
 		let theRain = raining();
@@ -182,7 +103,7 @@ function internMove(){
 			return "finito";
 		}else {
 			//console.log("the rain",theRain);
-			return [["raining :",theRain]];
+			return [["raining :",lstColor[theRain]]];
 		}
 	}
 	//console.log("out",out);
