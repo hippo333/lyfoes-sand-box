1021a5a31bbade6de18539854e14ac950ecbb8b3
hippo333 <junk.mail.65.hippo@gmail.com>
2024-04-01 18:07:33 +0200

    if we ave only one ball above the ball on the first column with the ball

--- justBlue.js
+++ justBlue.js
@@ -13,18 +13,20 @@ function locateBlue(a){
 	let colorTgt = lstBigBall.findIndex(
 		tgt => tgt[1] == a
 	);
-		
+	//get index of all column who contain a	
 	columns.filter(function (cll, index) {
 		//condition for go to the list
    		if (cll.indexOf(a) != -1 
    		//exept one who as color to
-   		&& cll != colorTgt){
+   		&& cll != columns[colorTgt]){
    		    lst.push(index);
    		    return true;
   		}
 	});
-	console.log("lst",a,lst)
-	return lst
+	if(lst != -1){
+		//console.log("lst",a,lst)
+		return lst
+	}
 }
 
 //get the ball above the column
@@ -68,28 +70,57 @@ function Target(a){
 function raining(a){
 	let lstColBlue= locateBlue(a);
 	let target = Target(a);
+	
 	if (target == null){
 		console.log("i didn't get the target for ",a);
 		return
 	}
+	
 	let theCol;
+	let itWork = false;
 	for(way in lstColBlue){
 		theCol = lstColBlue[way];
 		if (topBall(theCol) == a){
+			console.log("it work",theCol,target);
 			move(theCol,target);
+			itWork = true;
 		}
 	}
+	if (!itWork){
+		//do it for only the first column who contain the ball
+		let thePbCol = lstColBlue[0]
+		let b = topBall(thePbCol);
+		let target2 = Target(b);
+		
+		//if it go to the same place
+		if (target == target2){return}
+		
+		//if a is the second ball
+		if(columns[thePbCol][columns[thePbCol].length -1 -lstBigBall[thePbCol][0]] == a){
+			move(thePbCol,target2);
+		}
+	}
+	
+	
+	
+	
 	console.log("// raining :",a);
 }
 raining(5);
-console.log(columns);
+console.log(columns);/*
 raining(2);
+console.log(columns);*/
+raining(5);
 console.log(columns);
 raining(5);
 console.log(columns);
-raining(7);
+raining(2);
 console.log(columns);
 raining(2);
+console.log(columns);
+raining(2);
+console.log(columns);
+raining(7);
 console.log(columns);
 
 
