e6135fdf298d1551200a82a76e6b670381246d0e
hippo333 <junk.mail.65.hippo@gmail.com>
2024-08-30 18:07:44 +0200

    beter error log

--- move.js
+++ move.js
@@ -8,7 +8,7 @@ function move(state,from,to){
 	let [columns3,lstBigBall3,lstOfMove3] = state;
 	
 	if(from ==-1 || to ==-1){
-		console.log("there are a probleme with the move",from,to);
+		throw new error("from",from,"to",to);
 	}
 	
 	let colFrom = columns3[from];
@@ -19,22 +19,22 @@ function move(state,from,to){
 	
 	
 	if(columns3[to].length ==4){
-		console.log("error the column",to,"is full",colTo)
+		throw new error("col is overfeeding",to,colTo);
 		return "error"
 		//the botle to is not empty
 	}else if(ballTo != undefined){
 		if(ballFrom != ballTo){
-			console.log("error the ball are diferent",colFrom,colTo);
+			throw new error("the ball are diferent",colFrom,colTo);
 			return "error"
 		}
 	}
 	if(ballFrom == undefined){
-		console.log("error no ball From",from,colFrom);
+		throw new error("no ball From",from,colFrom);
 		return "error"
 	}
 	if(colTo.length + lstBigBall3[from][0] >4){
 		console.log("arg");
-		console.log("lstBigBall from",lstBigBall3[from]);
+		throw new error("lstBigBall from",lstBigBall3[from]);
 		return "error";
 	}
 	//console.log("la colune from contien",lstBigBall3[from][0]);
