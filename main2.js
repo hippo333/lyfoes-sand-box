ed90b3772e2390148dc1c47c769ef2c7c8fbde08
hippo333 <junk.mail.65.hippo@gmail.com>
2025-04-12 19:00:32 +0200

    moveTo is a function and we stop when we can't move

--- main2.js
+++ main2.js
@@ -19,7 +19,7 @@ function emptyBotle(columns2){
 }
 
 function WhoCanGoTo(Vcolumn2,col2){
-	//console.log("who can go to ",col2);
+	//console.log("    who can go to ",col2);
 	let output = [];
 	
 	for(let i=0;i<Vcolumn2.length;i++){
@@ -35,6 +35,33 @@ function WhoCanGoTo(Vcolumn2,col2){
 	}
 	return output
 }
+
+function moveTo([Vcolumn2,lstOfMove2]){	
+	//console.log("  moveTo");
+	let output = [];
+	
+	colTo =lstOfMove2[lstOfMove2.length-1][0];
+	let lst = WhoCanGoTo(Vcolumn2,colTo);
+	//console.log("  lst",lst);
+	
+	//add to the list for the next Cycle
+	for(k in lst){
+		let Vcolumn4 = [...Vcolumn2];
+		let lstOfMove4 = [...lstOfMove2];
+		colFrom = lst[k];
+	
+		Vupdate(columns,Vcolumn4,[colFrom,colTo])
+		lstOfMove4.push([colFrom,colTo]);
+		
+		//console.log("  push",Vcolumn4,"\n",lstOfMove4);
+		//console.log(" ",colFrom,colTo);
+		output.push([Vcolumn4,lstOfMove4]);
+	}
+	return output
+}
+
+
+
 
 let lstOfSolution =[];	//output of the function
 let lstOfMove = [];
@@ -58,6 +85,7 @@ lstOfMove.push([From0,To0]);
 console.log(Vcolumn);
 		
 		
+		
 for(let i=0;i<4;i++){
 	console.log("\ncycle",i);
 	let Vlist2 =[];
@@ -70,33 +98,15 @@ for(let i=0;i<4;i++){
 			lstOfSolution.push(lstOfMove3);
 			continue;	//add to the output
 		}
-
-		colTo =lstOfMove3[lstOfMove3.length-1][0];
-		console.log("last move",lstOfMove3[lstOfMove3.length-1]);
-		console.log("colTo",colTo);
-		let lst = WhoCanGoTo(Vcolumn3,colTo);
-		console.log("lst",lst);
 		
-		//add to the list for the next Cycle
-		for(k in lst){
-			let Vcolumn4 = [...Vcolumn3];
-			let lstOfMove4 = [...lstOfMove3];
-			colFrom = lst[k];
-		
-			Vupdate(columns,Vcolumn4,[colFrom,colTo])
-			lstOfMove4.push([colFrom,colTo]);
-			console.log("move from",colFrom,"to",colTo);
-			console.log(Vcolumn4);
-			console.log(lstOfMove4);
-			
-			
-			Vlist2.push([Vcolumn4,lstOfMove4]);
-		}
-		
+		let MoveTo = moveTo(Vlist[j]);
+		Vlist2 = Vlist2.concat(MoveTo);
 	}
 
 	Vlist = Vlist2;	//for the next cycle
 	console.log("Vlist",Vlist.length);
+	
+	if(Vlist == []){break}
 }
 
 console.log("\n\n\nthe solution");
