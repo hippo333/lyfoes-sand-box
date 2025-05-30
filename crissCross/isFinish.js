570c2fc1740e1274355ac6aaa7c06c2f4f1f815d
hippo333 <junk.mail.65.hippo@gmail.com>
2025-05-30 16:52:52 +0200

    new way to calcul

---
+++ crissCross/isFinish.js
@@ -0,0 +1,32 @@
+var columns = require('../level');
+var abstract = require('../tools/abstract');
+
+function isMonochrome(col2){
+	let differentBallInCol = [... new Set(col2.content)];
+	
+	if(!Array.isArray(differentBallInCol)){
+		console.log("monochrom error for the col");
+		console.log(col2.content);
+		return null
+	}
+	if(differentBallInCol.length >1){return false}
+	
+	else {
+	return true}
+}
+
+function isFinish(columns2){
+	for(let i=0;i<columns2.length;i++){
+		let isMono = isMonochrome(columns2[i]);
+		if(isMono == false){return false}
+		
+		if(isMono == null){
+			console.log("isFinish error in the col",i);
+			abstract(columns2);
+			return
+		}
+	}
+	return true
+}
+
+module.exports = isFinish
