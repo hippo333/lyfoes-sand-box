bc2e206544f6f13191daaa5b1b45c0082ff6a42d
hippo333 <junk.mail.65.hippo@gmail.com>
2025-10-12 18:24:48 +0200

    clean the code

--- newApproach/aveAWay.js
+++ newApproach/aveAWay.js
@@ -12,6 +12,27 @@ function dejaVu(previousTry2,col2,level2){
 	return true
 }
 
+//add element at the end of each lstOfMove in theArray
+function mapArray(theArray,element){
+	theArray2 = [];
+	theArray.forEach(
+		lstMv => lstMv.push(element)
+		&& theArray2.push(lstMv)
+	);
+	theArray = theArray2;
+	return theArray2
+}
+
+
+function badMove(col2,level2,previousTry2){
+	let theCol = columns[col2]; 
+	if(theCol.isEmpty()){return true}
+	if(level2 ==-1){return true}//theCol dont ave the ball
+	if(level2 == 3){return true}//no place above
+	if(dejaVu(previousTry2,col2,level2)){return true}
+	return false
+}
+
 
 function aveAWay(col2,ball2,nbCycle,previousTry){
 	console.log("ave a Way col",col2,"ball",ball2);
@@ -27,57 +48,43 @@ function aveAWay(col2,ball2,nbCycle,previousTry){
 		let theCol = columns[col];
 		//console.log("col",col);
 		
-		if(theCol.isEmpty()){continue}
-		
 		let level = theCol.content.lastIndexOf(ball2);
-		//console.log("level",level);
-		if(level ==-1){continue}
-		if(level ==3){continue}//no place for the ball
 		
-		if(dejaVu(previousTry,col,level)){continue}
-	
+		if(badMove(col,level,previousTry)){continue}
+		
 		if(theCol.top() == ball2){
-			//return [[col2],[col]]
 			outputLst.push([[col2,col]]);
 			console.log("outputList add top",outputLst);
 			continue;
 		}
 		
-		//arbitrary
-		if(level ==0){continue}
-		previousTry2 = [...previousTry];
-		previousTry2.push([col,level]);
+		previousTry2 = [...previousTry,[col,level]];
 		
 		if(level >=1){
 			let thisList =[]
 			for(let bll=theCol.content.length-1;bll >1;bll--){
 				let secondMove = aveAWay(col,theCol.content[bll],++nbCycle,previousTry2);
+				//if(typeof(secondMove = "undefined"){continue columns0}
 				if(secondMove.length == 0){continue columns0}
 				
 				if(thisList.length ==0){
 					thisList.push(...secondMove);
 				}else{
-					let thisList2 = [];
-					thisList.forEach(x => x.push(...(secondMove[0]))&& thisList2.push(x));
-					thisList = thisList2;
+					mapArray(thisList,...secondMove[0]);
 				}
 			}
 			let theMv = [col2,col]
-			console.log("thisList befor",thisList);
-			let thisList2 = [];
-			thisList.forEach(x => x.push(theMv) && thisList2.push(x));
-			thisList = thisList2;
-			console.log("thisList after",thisList);
-			//return thisList
+			mapArray(thisList,theMv);
+			
 			outputLst.push(...thisList);
 		}
-		
-		
-		
 	}
 	console.log("outputLst",outputLst,"\n");
-	return outputLst	//no way
+	return outputLst
+	//return outputLst[0]	//
 }
+
+
 abstract(columns);
 let firstCol = 2;
 
@@ -85,15 +92,16 @@ let firstCol = 2;
 let firstLevel = columns[firstCol].content.length-1;
 let theBall = columns[firstCol].top();	//top
 
-let theSolution = aveAWay(firstCol,theBall,0,[[firstCol,firstCol]]);
+let theSolution = aveAWay(firstCol,theBall,0,[[firstCol,-1]]);
 
 console.log("the solution",...theSolution);
 
 
 
+/*
+when the bigBall seems stuck the game, we search all solution who can consume the secondBall befor we move the big ball
+aveAWay clear the path and leave an empty botle for the bigBall then the second ball(i asume it is the botom one) go to the place we make for it
+the output is a list of all path posible
 
-
-
-
-
+*/
 
